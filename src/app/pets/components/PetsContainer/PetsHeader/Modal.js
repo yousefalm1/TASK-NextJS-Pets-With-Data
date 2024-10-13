9266fa217f2b2830a0b14a638873d045b707d575
Octowl <alsaff1987@gmail.com>
2024-10-13 21:32:14 +0300

    cleanup modal and input

--- src/app/pets/components/PetsContainer/PetsHeader/Modal.js
+++ src/app/pets/components/PetsContainer/PetsHeader/Modal.js
@@ -2,12 +2,27 @@ import Input from "@/components/Input";
 import React, { useState } from "react";
 
 function Modal() {
-  const [name, setName] = useState("");
-  const [type, setType] = useState("");
-  const [image, setImage] = useState("");
-  const [available, setAvailable] = useState(0);
+  const initialPetState = {
+    name: "",
+    type: "",
+    image: "",
+    available: true
+  }
 
+  const [pet, setPet] = useState(initialPetState)
   const [show, setShow] = useState(false);
+
+  function handleChange(e) {
+    setPet({
+      ...pet,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  function handleSubmit() {
+    setPet(initialPetState)
+    setShow(false)
+  }
 
   if (!show) return (
     <button
@@ -21,48 +36,40 @@ function Modal() {
   );
 
   return (
-    <div
-      className="inset-0 fixed  flex justify-center items-center flex-col z-20 overflow-hidden "
-    >
+    <div className="inset-0 fixed  flex justify-center items-center flex-col z-20 overflow-hidden">
       <div className="bg-black absolute z-0 opacity-70 inset-0 "></div>
-      <div className="relative z-10 flex flex-col gap-3 border-[3px] border-black rounded-md w-[95%] md:w-[40%] h-[300px] md:h-[30%] bg-white pt-[50px]">
+      <form
+        onSubmit={handleSubmit}
+        className="relative z-10 flex flex-col gap-3 border-[3px] border-black rounded-md w-[95%] md:w-[40%] h-[300px] md:h-[30%] bg-white pt-[50px]"
+      >
         <button
           className="right-0 top-2 absolute w-[70px] border border-black rounded-md ml-auto mr-5 hover:bg-red-400"
-          onClick={() => {
-            setShow(false);
-          }}
+          onClick={() => setShow(false)}
         >
           Close
         </button>
         <Input
           title="Pet Name"
-          onChange={(e) => {
-            setName(e.target.value);
-          }}
+          name="name"
+          value={pet.name}
+          onChange={handleChange}
         />
         <Input
           title="Pet Type"
-          onChange={(e) => {
-            setType(e.target.value);
-          }}
+          name="type"
+          value={pet.type}
+          onChange={handleChange}
         />
         <Input
           title="Pet Image Link"
-          onChange={(e) => {
-            setImage(e.target.value);
-          }}
+          name="image"
+          value={pet.image}
+          onChange={handleChange}
         />
-        <Input
-          title="Pet adopted?"
-          onChange={(e) => {
-            setAvailable(e.target.value);
-          }}
-        />
-
-        <button className="w-[70px] border border-black rounded-md ml-auto mr-5 hover:bg-green-400">
+        <button type="submit" className="w-[70px] border border-black rounded-md ml-auto mr-5 hover:bg-green-400">
           Submit
         </button>
-      </div>
+      </form>
     </div>
   );
 };
--- src/components/Input.js
+++ src/components/Input.js
@@ -1,9 +1,10 @@
-function Input({ title, onChange, name }) {
+function Input({ title, onChange, name, value }) {
   return (
     <div className="flex justify-center ">
       <h1 className="w-[25%]">{title}</h1>
       <input
         name={name}
+        value={value}
         onChange={onChange}
         className="w-[70%] border pl-2 border-black rounded-md"
         placeholder={title}
