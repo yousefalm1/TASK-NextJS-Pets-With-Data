5575e48f6a496b33dcebde5cc10035f301f1e617
Octowl <alsaff1987@gmail.com>
2024-10-13 21:59:03 +0300

    pet detail page

---
+++ src/app/pets/[id]/page.js
@@ -0,0 +1,41 @@
+import pets from "@/data/pets";
+import Image from "next/image";
+import { redirect } from "next/navigation";
+
+const PetDetailPage = ({ params }) => {
+  const pet = pets.find(pet => pet.id === +params.id);
+
+  if (!pet) redirect('/pets');
+
+  const { image, name, type, adopted } = pet;
+  return (
+    <div className="w-screen h-[100vh] flex justify-center items-center">
+      <div className="border border-black rounded-md w-[70%] h-[70%] overflow-hidden flex flex-col md:flex-row p-5">
+        <div className="h-full w-full md:w-[35%]">
+          <Image
+            src={image}
+            alt={name}
+            className="object-contain w-full h-full"
+            width={200}
+            height={200}
+          />
+        </div>
+        <div className="w-full md:w-[65%] h-full pt-[30px] flex flex-col p-3">
+          <h1>Name: {name}</h1>
+          <h1>Type: {type}</h1>
+          <h1>adopted: {adopted ? "yes" : "no"}</h1>
+
+          {!adopted && <button className="w-[70px] border border-black rounded-md  hover:bg-green-400 my-5">
+            Adobt
+          </button>}
+
+          <button className="w-[70px] border border-black rounded-md  hover:bg-red-400">
+            Delete
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PetDetailPage;
