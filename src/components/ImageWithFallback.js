4c3f5ab600c9e2b62184e8b57beead597b6bc14d
Octowl <alsaff1987@gmail.com>
2024-10-14 10:54:45 +0300

    use fallback on error

--- src/components/ImageWithFallback.js
+++ src/components/ImageWithFallback.js
@@ -1,8 +1,16 @@
+'use client'
+
 import placeholder from '@/images/placeholder.jpg'
 import Image from "next/image"
+import { useState } from 'react'
 
 function ImageWithFallback(props) {
-  return <Image {...props} src={props.src.startsWith("http") ? props.src : placeholder} />
+  const [src, setSrc] = useState(props.src.startsWith("http") ? props.src : placeholder)
+  return <Image
+    {...props}
+    src={src}
+    onError={() => setSrc(placeholder)}
+  />
 
 }
 
