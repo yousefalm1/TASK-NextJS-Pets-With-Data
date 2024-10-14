e1db558a808de881c9a021ec59016e8ab21dc4d7
Octowl <alsaff1987@gmail.com>
2024-10-14 09:35:13 +0300

    styling

--- src/app/pets/[id]/page.js
+++ src/app/pets/[id]/page.js
@@ -9,7 +9,7 @@ const PetDetailPage = ({ params }) => {
 
   const { image, name, type, adopted } = pet;
   return (
-    <div className="w-screen h-[100vh] flex justify-center items-center">
+    <div className="w-screen h-[90vh] flex justify-center items-center">
       <div className="border border-black rounded-md w-[70%] h-[70%] overflow-hidden flex flex-col md:flex-row p-5">
         <div className="h-full w-full md:w-[35%]">
           <Image
